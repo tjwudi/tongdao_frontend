9518c09b0dc9c2e3455a5d8c4747a928bab61001
John Wu <webmaster@leapoahead.com>
2013-11-27 20:32:03 +0800

    regular commit

--- controllers/main.js
+++ controllers/main.js
@@ -4,9 +4,14 @@ main.index = function(req, res){
   if (! req.session.user) {
     return res.redirect('/welcome');
   }
+  
   res.end();
 }
 
 main.welcome = function(req, res){
+  if (!! req.session.user) {
+    return res.redirect('/');
+  }
+
   res.render('welcome.html');
 }
